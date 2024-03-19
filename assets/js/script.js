5ea625462197c24c7112a812344188f54c52d3bd
Eider Agudelo <eider841agudelo@hotmail.com>
2024-03-19 16:55:57 -0300

    Se agrega tooltips, modal y alert

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,34 @@
-import '../../node_modules/bootstrap/dist/js/bootstrap.bundle.js'
+import '../../node_modules/bootstrap/dist/js/bootstrap.bundle.js';
+
+/** Tooltip */
+const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]');
+const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl));
+
+/** Modal */
+const myModal = document.getElementById('myModal');
+const myInput = document.getElementById('myInput');
+
+myModal.addEventListener('shown.bs.modal', () => {
+    myInput.focus()
+})
+
+/** Alert */
+const alertPlaceholder = document.getElementById('liveAlertPlaceholder')
+const appendAlert = (message, type) => {
+    const wrapper = document.createElement('div')
+    wrapper.innerHTML = [
+        `<div class="alert alert-${type} alert-dismissible" role="alert">`,
+        `   <div>${message}</div>`,
+        '   <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>',
+        '</div>'
+    ].join('')
+
+    alertPlaceholder.append(wrapper)
+}
+
+const alertTrigger = document.getElementById('liveAlertBtn')
+if (alertTrigger) {
+    alertTrigger.addEventListener('click', () => {
+        appendAlert('Tu mensaje se ha enviado con exito', 'success')
+    })
+}
